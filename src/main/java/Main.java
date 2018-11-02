08de2a08f233551121d3724182efa0c6f71171f2
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-11-02 14:40:40 +0200

    Finish interactive gradle tutorial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.util.*;
+import ohtu.Multiplier;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello gradle!");
+        Multiplier kolme = new Multiplier(3);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Anna luku");
+            int luku = scanner.nextInt();
+
+            System.out.println("luku kertaa kolme on " + kolme.multipliedBy(luku));
+        }
     }
 }
